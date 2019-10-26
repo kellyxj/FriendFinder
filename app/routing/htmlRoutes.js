b7df9cc133ce69c738cd80b12b2b26a1553a5bbb
Kelly Jiang <kllyjiang@gmail.com>
2019-10-26 04:46:00 -0500

    Adding package.json and fixing paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.render("../public/home.html");
+    res.sendFile(path.join(__dirname,"../public/home.html"));
 });
 
-router.get("/", (req,res) => {
-    res.render("../public/survey.html");
+router.get("/survey", (req,res) => {
+    res.sendFile(path.join(__dirname,"../public/survey.html"));
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,8 +6,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const htmlRoutes = require("./routing/htmlRoutes.js");
-const apiRoutes = require("./routing/apiRoutes.js");
+const htmlRoutes = require("./app/routing/htmlRoutes.js");
+const apiRoutes = require("./app/routing/apiRoutes.js");
 
 app.use(htmlRoutes, apiRoutes);
 
