070e386536468952b86d8d832869948c77e69d0e
Kelly Jiang <kllyjiang@gmail.com>
2019-10-26 04:29:49 -0500

    Adding html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    res.render("../public/home.html")
+});
+
+router.get("/", (req,res) => {
+    res.render("../public/survey.html");
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const htmlRoutes = require("./routing/htmlRoutes.js");
+const apiRoutes = require("./routing/apiRoutes.js");
+
+app.use(htmlRoutes, apiRoutes);
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
