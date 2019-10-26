d50805571b37069d4565a33d67154c0d3ba58219
Kelly Jiang <kllyjiang@gmail.com>
2019-10-26 04:34:07 -0500

    Adding api routes and initial html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const friendsData = require("../data/friends.js");
+
+const router = express.Router();
+
+router.get("/api/friends", (req,res) => {
+
+});
+
+router.post("/api/friends", (req,res) => {
+
+});
+
+module.exports = router;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.render("../public/home.html")
+    res.render("../public/home.html");
 });
 
 router.get("/", (req,res) => {
