f5fc9cd3505232c1fdd851f9afd874e29927bb98
Kelly Jiang <kllyjiang@gmail.com>
2019-10-26 21:13:33 -0500

    Using process.env.HOST

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || "localhost";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +12,6 @@ const apiRoutes = require("./app/routing/apiRoutes.js");
 
 app.use(htmlRoutes, apiRoutes);
 
-app.listen(PORT, function() {
+app.listen(PORT, HOST, function() {
     console.log("App listening on PORT " + PORT);
 });
