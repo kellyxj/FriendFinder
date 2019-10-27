428bb6ee0c75b962b61e222f01bc3f569c7a810a
Kelly Jiang <kllyjiang@gmail.com>
2019-10-26 20:38:00 -0500

    Adding dummy content to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+const friends = [
+    {
+        name: "Strong Agree",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "Hard Disagree",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Smashthat Threebutton",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    }
+];
+
+module.exports = friends;
